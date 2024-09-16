524934e682904af68e954fa7837a8a3006b74a7c
dev Anderson Roberto <ander9854@gmail.com>
2024-09-16 14:50:07 -0300

    change server url

--- frontend/.js/script.js
+++ frontend/.js/script.js
@@ -149,7 +149,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://new-chat-09qv.onrender.com")
     websocket.onmessage = processMessage
 }
 
